46e2d36302b45ae18da2a39d4a5609aac472ceed
sanjay <sanjaykushites@gmail.com>
2022-12-20 17:24:16 +0530

    aplication.properties setup

--- StudentManagement/src/main/java/com/studentmanagement/entity/Student.java
+++ StudentManagement/src/main/java/com/studentmanagement/entity/Student.java
@@ -3,10 +3,14 @@ package com.studentmanagement.entity;
 import java.sql.Time;
 import java.util.List;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,7 +26,9 @@ public class Student {
 	private String name;
 	private Time dob;
 	private String gender;
+	@ManyToMany
 	private List<Course> courses;
+	@OneToMany
 	private List<Address> addresses;
 	
 	
